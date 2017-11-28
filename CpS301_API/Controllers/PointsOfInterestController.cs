74d1abebc1a6212c0b82d79f9c2904db211b776c
JulCesWhat <julceswhat@gmail.com>
2017-11-28 17:59:37 -0500

    Adding dummy code to understand how the api works.

---
+++ CpS301_API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,68 @@
+﻿using CpS301_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CpS301_API.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            // Find city
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(city.PointsOfInterest);
+            }
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{pointId}", Name = "GetPointOfInterest")]
+        public IActionResult GetPointOfInterest(int cityId, int pointId)
+        {
+            // Find city
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointId);
+
+            if(pointOfInterest == null)
+            {
+                return NotFound();
+                
+            }
+
+            return Ok(pointOfInterest);
+        }
+
+        [HttpPost("{cityId}/pointofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if(pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            //NEED TO STILL FINISH THIS
+            return CreatedAtRoute("GetPointOfInterest", );
+        }
+    }
+}
