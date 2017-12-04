a04e182387d0ffc686f247b5335547050536b3ab
julceswhat <julceswhat@gmail.com>
2017-12-04 01:20:55 -0800

    Adding final touches.

---
+++ CpS301_API/Models/viewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CpS301_API.Models
+{
+    public class viewClass
+    {
+        public int SeqNum { get; set; }
+        public string Description { get; set; }
+        public string Arranger { get; set; }
+    }
+}
